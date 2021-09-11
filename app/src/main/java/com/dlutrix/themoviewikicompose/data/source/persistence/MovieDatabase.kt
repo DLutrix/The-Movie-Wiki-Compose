b1d1018d056dac0c3a3d00cb275d636576d36d3c
Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>
2021-09-11 10:39:26 +0800

    Extend RoomDatabase()

--- app/src/main/java/com/dlutrix/themoviewikicompose/data/source/persistence/MovieDatabase.kt
+++ app/src/main/java/com/dlutrix/themoviewikicompose/data/source/persistence/MovieDatabase.kt
@@ -1,13 +1,15 @@
 package com.dlutrix.themoviewikicompose.data.source.persistence
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.dlutrix.themoviewikicompose.data.entity.Movie
+import com.dlutrix.themoviewikicompose.data.entity.UpcomingMoviesRemoteKey
 
 @Database(
-    entities = [Movie::class, UpcomingMoviesRemoteKeyDao::class],
+    entities = [Movie::class, UpcomingMoviesRemoteKey::class],
     version = 1
 )
-abstract class MovieDatabase {
+abstract class MovieDatabase : RoomDatabase() {
     abstract fun upcomingMoviesDao(): UpcomingMoviesDao
     abstract fun upcomingMoviesRemoteKeyDao(): UpcomingMoviesRemoteKeyDao
     abstract fun nowPlayingMoviesDao(): NowPlayingMoviesDao
