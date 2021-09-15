d54201dff688ef8dd15b86000e5ee8b9c0a25921
Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>
2021-09-15 18:04:42 +0800

    Add MovieDetailRepository

--- app/src/main/java/com/dlutrix/themoviewikicompose/data/source/network/TMDBApiService.kt
+++ app/src/main/java/com/dlutrix/themoviewikicompose/data/source/network/TMDBApiService.kt
@@ -1,8 +1,11 @@
 package com.dlutrix.themoviewikicompose.data.source.network
 
+import com.dlutrix.themoviewikicompose.data.model.MovieDetail
 import com.dlutrix.themoviewikicompose.data.model.NowPlayingMovies
 import com.dlutrix.themoviewikicompose.data.model.UpcomingMovies
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TMDBApiService {
@@ -16,4 +19,10 @@ interface TMDBApiService {
     suspend fun getNowPlayingMovies(
         @Query("page") page: Int = 1
     ): NowPlayingMovies
+
+    @GET("movie/{movieId}")
+    suspend fun getMovieDetail(
+        @Path("movieId") movieId: Int,
+        @Query("append_to_response") appendToResponse: String = "images"
+    ): Response<MovieDetail>
 }
---
+++ app/src/main/java/com/dlutrix/themoviewikicompose/data/source/network/moviedetail/MovieDetailRepository.kt
@@ -0,0 +1,27 @@
+package com.dlutrix.themoviewikicompose.data.source.network.moviedetail
+
+import com.dlutrix.themoviewikicompose.data.model.MovieDetail
+import com.dlutrix.themoviewikicompose.data.source.network.TMDBApiService
+import com.dlutrix.themoviewikicompose.util.Resource
+import retrofit2.HttpException
+import java.io.IOException
+
+class MovieDetailRepository(
+    private val service: TMDBApiService
+) {
+
+    suspend fun getMovieDetail(movieId: Int): Resource<MovieDetail> {
+        return try {
+            val response = service.getMovieDetail(movieId)
+            if (response.isSuccessful) {
+                Resource.Success(response.body())
+            } else {
+                Resource.Error(null, Throwable("Failed to fetch data from server"))
+            }
+        } catch (e: IOException) {
+            Resource.Error(null, Throwable("Unexpected error"))
+        } catch (e: HttpException) {
+            Resource.Error(null, Throwable("No connection"))
+        }
+    }
+}
