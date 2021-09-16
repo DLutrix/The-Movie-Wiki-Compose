827ff3ddd2c0c55199787b12780e9ee62e54f1c6
Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>
2021-09-16 08:23:38 +0800

    Remove unnecessary packages

--- app/src/main/java/com/dlutrix/themoviewikicompose/ui/discover/NowPlayingMoviesPager.kt
+++ app/src/main/java/com/dlutrix/themoviewikicompose/ui/discover/NowPlayingMoviesPager.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -16,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -24,19 +21,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.lerp
 import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.dlutrix.themoviewikicompose.R
 import com.dlutrix.themoviewikicompose.data.entity.Movie
-import com.dlutrix.themoviewikicompose.ui.theme.BlackRs
-import com.dlutrix.themoviewikicompose.ui.theme.Shamrock
 import com.dlutrix.themoviewikicompose.ui.theme.SilverChalice
 import com.google.accompanist.pager.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
 
 @ExperimentalAnimationApi
 @ExperimentalCoilApi
